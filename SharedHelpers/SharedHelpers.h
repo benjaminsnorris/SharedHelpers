c1d20b48f139aaff438a383fd6fd90cd59111162
Ben Norris <code@bsn.design>
2016-06-23 09:09:26 -0600

    Add empty framework

---
+++ SharedHelpers/SharedHelpers.h
@@ -0,0 +1,19 @@
+//
+//  SharedHelpers.h
+//  SharedHelpers
+//
+//  Created by Ben Norris on 6/23/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedHelpers.
+FOUNDATION_EXPORT double SharedHelpersVersionNumber;
+
+//! Project version string for SharedHelpers.
+FOUNDATION_EXPORT const unsigned char SharedHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedHelpers/PublicHeader.h>
+
+
