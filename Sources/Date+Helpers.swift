ff0dd278468d846d93218efb3b23a930b8953ad5
Ben Norris <code@bsn.design>
2016-12-31 12:28:38 -0700

    Remove date rounding helper

--- Sources/Date+Helpers.swift
+++ Sources/Date+Helpers.swift
@@ -170,31 +170,7 @@ public extension Date {
     
     
     // MARK: - Functions
-    
-    /**
-     Rounds down a date by the specified minutes. For use with `UIDatePicker` to
-     determine the display date when using a `minuteInterval` greater than 1.
-     
-     - parameter minutes: Number of minutes to use in rounding. When used together
-        with `UIDatePicker`, this should match the `minuteInterval`.
-     */
-    public func rounded(minutes: Int) -> Date {
-        let calendar = NSCalendar.current
-        var components = calendar.dateComponents([.minute], from: self)
-		guard let originalMinutes = components.minute else { return self }
-        let extraMinutes = originalMinutes % minutes
-        let rounded = extraMinutes / minutes
-        let adjustedMinutes: Int
-        if rounded == 0 {
-            adjustedMinutes = -extraMinutes
-        } else {
-            adjustedMinutes = minutes - extraMinutes
-        }
-        components.minute = adjustedMinutes
-        guard let roundedDate = calendar.date(byAdding: components, to: self) else { return self }
-        return roundedDate
-    }
-    
+        
     public func isSameDay(as date: Date) -> Bool {
         let calender = Calendar.current
         let components: Set<Calendar.Component> = [.day, .month, .year]
