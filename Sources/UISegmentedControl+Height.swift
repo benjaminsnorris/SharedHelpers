6a5ada330fdf305c883b5fdc45734d9ab48086c8
Ben Norris <code@bsn.design>
2017-02-27 10:12:32 -0700

    Add segmented control recommended height helper

---
+++ Sources/UISegmentedControl+Height.swift
@@ -0,0 +1,25 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import UIKit
+
+public extension UISegmentedControl {
+    
+    public func recommendedHeight(with padding: CGFloat) -> CGFloat? {
+        var label: UILabel?
+        guard let segment = subviews.first else { return nil }
+        for subview in segment.subviews {
+            if subview.isKind(of: UILabel.self) {
+                label = subview as? UILabel
+            }
+        }
+        guard let height = label?.frame.height else { return nil }
+        return height + (padding * 2)
+    }
+
+    
+}
