71fa5e28091cafa2b67ca629451d445abcaa624f
Ben Norris <code@bsn.design>
2017-02-27 11:19:45 -0700

    Move over properties for image view and view

--- Sources/CustomImageView.swift
+++ Sources/CustomImageView.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-@IBDesignable open class CustomImageView: UIImageView, CircularView {
+@IBDesignable open class CustomImageView: UIImageView, CircularView, BackgroundColorNameable, TintColorNameable, BorderColorNameable {
     
+    // MARK: - Inspectable properties
+    
+    @IBInspectable open var backgroundColorName: String? {
+        didSet {
+            applyBackgroundColorName()
+        }
+    }
+    
+    @IBInspectable open var tintColorName: String? {
+        didSet {
+            applyTintColorName()
+        }
+    }
+    
+    @IBInspectable open var borderColorName: String? {
+        didSet {
+            applyBorderColorName()
+        }
+    }
+
     @IBInspectable open var circular: Bool = false {
         didSet {
             layoutSubviews()
         }
     }
+    
+    
+    // MARK: - Lifecycle overrides
     
     override open func layoutSubviews() {
         super.layoutSubviews()
--- Sources/CustomView.swift
+++ Sources/CustomView.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-@IBDesignable open class CustomView: UIView, CircularView {
+@IBDesignable open class CustomView: UIView, CircularView, BackgroundColorNameable, TintColorNameable, BorderColorNameable {
+    
+    // MARK: - Inspectable properties
+    
+    @IBInspectable open var backgroundColorName: String? {
+        didSet {
+            applyBackgroundColorName()
+        }
+    }
+    
+    @IBInspectable open var tintColorName: String? {
+        didSet {
+            applyTintColorName()
+        }
+    }
+    
+    @IBInspectable open var borderColorName: String? {
+        didSet {
+            applyBorderColorName()
+        }
+    }
     
     @IBInspectable open var circular: Bool = false {
         didSet {
             layoutSubviews()
         }
     }
+    
+    
+    // MARK: - Lifecycle overrides
     
     override open func layoutSubviews() {
         super.layoutSubviews()
