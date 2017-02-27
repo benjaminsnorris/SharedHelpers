8971a098728cf8a3127472f5ce5d60e3afeb24f1
Ben Norris <code@bsn.design>
2017-02-27 10:29:02 -0700

    Add timezone helper to display current

---
+++ Sources/TimeZone+CurrentName.swift
@@ -0,0 +1,20 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import Foundation
+
+public extension TimeZone {
+    
+    public func localizedRelativeName(for style: NSTimeZone.NameStyle, locale: Locale?) -> String? {
+        if self == TimeZone.current, let shortName = localizedName(for: .shortStandard, locale: locale) {
+            return String.localizedStringWithFormat(NSLocalizedString("Current (%@)", comment: "Display for current time zone. Parameter is timezone abbreviation, e.g. Current (MST)"), shortName)
+        } else {
+            return localizedName(for: style, locale: locale)
+        }
+    }
+    
+}
