93dd0e426d901c934f9720d1fda9cb4648afa8ec
Ben Norris <code@bsn.design>
2017-03-16 16:18:08 -0600

    Add haptic feedback to cancel action

--- Sources/UIAlertAction+Cancel.swift
+++ Sources/UIAlertAction+Cancel.swift
@@ -10,7 +10,11 @@ import UIKit
 public extension UIAlertAction {
     
     public class var cancel: UIAlertAction {
-        return UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel button title"), style: .cancel, handler: nil)
+        return UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel button title"), style: .cancel) { _ in
+            if #available(iOSApplicationExtension 10.0, *) {
+                UISelectionFeedbackGenerator().selectionChanged()
+            }
+        }
     }
     
 }
