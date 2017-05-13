9c190f265947751e01c45ea1776f11b8ace58f4b
Ben Norris <code@bsn.design>
2017-05-13 08:06:01 -0600

    Tweak haptic for cancel button

--- Sources/UIAlertAction+Cancel.swift
+++ Sources/UIAlertAction+Cancel.swift
@@ -11,8 +11,8 @@ public extension UIAlertAction {
     
     public class var cancel: UIAlertAction {
         return UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel button title"), style: .cancel) { _ in
-            if #available(iOSApplicationExtension 10.0, *) {
-                UISelectionFeedbackGenerator().selectionChanged()
+            if #available(iOS 10.0, *) {
+                UIImpactFeedbackGenerator(style: .light).impactOccurred()
             }
         }
     }
