9971a3f3f5cd7f6ce09c059636f5f23cf1409188
Ben Norris <code@bsn.design>
2017-07-18 17:12:06 -0600

    Add helpers for table view

---
+++ Sources/UITableView+Helpers.swift
@@ -0,0 +1,31 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import UIKit
+
+public extension UITableView {
+    
+    /// Reload table view and reselect any selected index paths
+    ///
+    /// - Parameter scrollPosition: Default value is `.none`
+    func reloadDataPreservingSelection(at scrollPosition: UITableViewScrollPosition = .none) {
+        let selectedIndexPaths = indexPathsForSelectedRows
+        reloadData()
+        if let selected = selectedIndexPaths {
+            selected.forEach { selectRow(at: $0, animated: false, scrollPosition: scrollPosition) }
+        }
+    }
+    
+    /// Deselect all selected cells
+    ///
+    /// - Parameter animated: Deselect cells using animation
+    func deselectAll(animated: Bool) {
+        guard let selected = indexPathsForSelectedRows else { return }
+        selected.forEach { deselectRow(at: $0, animated: animated) }
+    }
+    
+}
