dbb246b7e015188190bc7f8354455a6de896f9a0
Ben Norris <code@bsn.design>
2017-07-28 11:43:39 -0600

    Add extension for table view cells

---
+++ Sources/UITableViewCell+Helpers.swift
@@ -0,0 +1,18 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import UIKit
+
+public extension UITableViewCell {
+    
+    /// Hides cell separator by setting the left edge insets
+    /// to match the cell width
+    public func hideSeparator() {
+        separatorInset = UIEdgeInsets(top: 0, left: bounds.width, bottom: 0, right: 0)
+    }
+    
+}
