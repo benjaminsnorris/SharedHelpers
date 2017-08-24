e175ef8d4c56b1b105b1acc0061b935d18d0235c
Ben Norris <code@bsn.design>
2017-08-24 17:07:36 -0600

    Add function to adjust for existing keyboard frame

--- Sources/KeyboardAdjusting.swift
+++ Sources/KeyboardAdjusting.swift
@@ -32,24 +32,23 @@ public extension KeyboardAdjusting where Self: UIViewController {
      The adjustment will be based on the height of the keyboard
      as contained in the notification payload.
      
-     - parameter notification: The `Notification` that is delivered containing
-     information about the keyboard.
+     - Note: The constraint to adjust is assumed to be at the bottom of the view
+     controller's view. If it is not at the bottom, unexpected results may occur.
+     
+     - Parameters:
+       - notification: The `Notification` that is delivered containing
+    information about the keyboard.
+       - constraint: _(Optional)_ An explicit constraint to adjust
+       - statusBarHeight: The height of the shared application status bar
+     (`UIApplication.shared.statusBarFrame.height`). Used for form sheet modals.
      */
     public func keyboardWillChange(_ notification: Notification, constraint: NSLayoutConstraint? = nil, statusBarHeight: CGFloat = 0.0) {
         guard let userInfo = notification.userInfo,
             let keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey] as? CGRect,
             let curveInt = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? UInt,
-            let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? Double,
-            let window = view.window,
-            let wrapper = view.superview
+            let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? Double
             else { return }
-        let adjustedFrame = wrapper.convert(wrapper.frame, to: window)
-        var maxY = adjustedFrame.maxY
-        if modalPresentationStyle == .formSheet || navigationController?.modalPresentationStyle == .formSheet {
-            maxY = wrapper.frame.height + statusBarHeight
-        }
-        let offset = maxY - keyboardFrame.origin.y
-        let adjustedConstant = max(offset, 0)
+        let adjustedConstant = self.adjustedConstant(for: keyboardFrame, statusBarHeight: statusBarHeight)
         let curve = UIViewAnimationOptions(rawValue: curveInt)
         UIView.animate(withDuration: duration, delay: 0.0, options: [curve], animations: {
             if let constraint = constraint {
@@ -59,6 +58,41 @@ public extension KeyboardAdjusting where Self: UIViewController {
             }
             self.view.layoutIfNeeded()
         }, completion: nil)
+    }
+    
+    /**
+     Call this function in something like `viewDidAppear` when the keyboard is already
+     showing.
+     
+     - Note: In order for this to work, you must be tracking the frame of the keyboard
+     outside of this view controller.
+    
+     - Parameters:
+       - keyboardFrame: Saved frame of the keyboard that is currently visible
+       - constraint: _(Optional)_ An explicit constraint to adjust
+       - statusBarHeight: The height of the shared application status bar
+       (`UIApplication.shared.statusBarFrame.height`). Used for form sheet modals.
+     */
+    public func adjustConstraint(for keyboardFrame: CGRect, constraint: NSLayoutConstraint? = nil, statusBarHeight: CGFloat = 0.0) {
+        let adjustedConstant = self.adjustedConstant(for: keyboardFrame, statusBarHeight: statusBarHeight)
+        let constraint = constraint ?? constraintToAdjust
+        constraint?.constant = adjustedConstant
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: [], animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func adjustedConstant(for keyboardFrame: CGRect, statusBarHeight: CGFloat = 0.0) -> CGFloat {
+        guard let window = view.window,
+            let wrapper = view.superview
+            else { return 0.0 }
+        let adjustedFrame = wrapper.convert(wrapper.frame, to: window)
+        var maxY = adjustedFrame.maxY
+        if modalPresentationStyle == .formSheet || navigationController?.modalPresentationStyle == .formSheet {
+            maxY = wrapper.frame.height + statusBarHeight
+        }
+        let offset = maxY - keyboardFrame.origin.y
+        return max(offset, 0)
     }
     
     /**
