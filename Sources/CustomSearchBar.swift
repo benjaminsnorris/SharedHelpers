ede0c0ee898ba3c5e76978408b3e935fd1842016
Ben Norris <code@bsn.design>
2017-09-14 10:43:03 -0600

    Add subclass for styling search bar

---
+++ Sources/CustomSearchBar.swift
@@ -0,0 +1,55 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import UIKit
+
+@IBDesignable open class CustomSearchBar: UISearchBar, TintColorNameable {
+    
+    // MARK: - Inspectable properties
+    
+    @IBInspectable open var barTintColorName: String? {
+        didSet {
+            updateColors()
+        }
+    }
+    
+    @IBInspectable open var tintColorName: String? {
+        didSet {
+            updateColors()
+        }
+    }
+    
+    
+    // MARK: - Initializers
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        registerForNotifications()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        registerForNotifications()
+    }
+    
+    public convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    
+    // MARK: - Functions
+    
+    func registerForNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateColors), name: Notification.Name.AppearanceColorsUpdated, object: nil)
+    }
+    
+    func updateColors() {
+        applyTintColorName()
+        barTintColor = UIColor(named: barTintColorName)
+    }
+
+}
