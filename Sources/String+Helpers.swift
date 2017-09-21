60444ed40fd50d88c5e160d294d3c5c6a73db377
Ben Norris <code@bsn.design>
2017-09-21 17:37:46 -0600

    Add helper to get height of string

---
+++ Sources/String+Helpers.swift
@@ -0,0 +1,18 @@
+/*
+ |  _   ____   ____   _
+ | | |‾|  ⚈ |-| ⚈  |‾| |
+ | | |  ‾‾‾‾| |‾‾‾‾  | |
+ |  ‾        ‾        ‾
+ */
+
+import UIKit
+
+public extension String {
+    
+    func height(with width: CGFloat, font: UIFont = UIFont.preferredFont(forTextStyle: .body)) -> CGFloat {
+        let nsString = self as NSString
+        let rect = nsString.boundingRect(with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude), options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        return ceil(rect.height)
+    }
+    
+}
