9e051d0544240780b362fe3050a1ddc20f3dfde3
Ben Norris <code@bsn.design>
2017-11-30 15:28:38 -0700

    Add helper for setting button images

--- Sources/UIButton+Helpers.swift
+++ Sources/UIButton+Helpers.swift
@@ -9,9 +9,16 @@ import UIKit
 
 public extension UIButton {
     
-    func setTitleWithoutFlashing(to title: String?, for state: UIControlState = .normal) {
+    func setTitleWithoutFlashing(to title: String?, for state: UIControlState = []) {
         UIView.performWithoutAnimation {
             setTitle(title, for: state)
+            layoutIfNeeded()
+        }
+    }
+    
+    func setImageWithoutFlashing(to image: UIImage?, for state: UIControlState = []) {
+        UIView.performWithoutAnimation {
+            setImage(image, for: state)
             layoutIfNeeded()
         }
     }
