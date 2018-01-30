2dfa1ee378879c1f1d72930ff25da2d4875d31a9
Ben Norris <code@bsn.design>
2018-01-30 08:13:21 -0700

    Add function to nib loadable

--- Sources/NibLoadable.swift
+++ Sources/NibLoadable.swift
@@ -22,9 +22,24 @@ public extension NibLoadable where Self: UIView {
         return UINib(nibName: Self.nibName, bundle: bundle)
     }
     
+    /**
+     Function to load custom view from corresponding nib file.
+     
+     - returns: `UIView` that is defined in the nib file
+     */
     public func loadFromNib() -> UIView {
         guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else { fatalError("Error loading \(self) from nib") }
         return view
+    }
+    
+    /**
+     Loads the custom view from the nib, and adds it as a subview to the `UIView` custom
+     subclass, constraining it to the full size of the view. 
+     */
+    public func initializeFromNib() {
+        let view = loadFromNib()
+        addSubview(view)
+        view.constrainFullSize()
     }
     
 }
