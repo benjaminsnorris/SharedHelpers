baec6522b62372528fe944ae19a1c11cbea5a040
Ben Norris <code@bsn.design>
2018-06-11 15:17:26 -0600

    Add function to apply accessibility identifiers
    Uses the property name of all relevant properties on the object. When used with a view controller, this will apply to its stored properties.

--- Sources/Nameable.swift
+++ Sources/Nameable.swift
@@ -15,4 +15,19 @@ public extension Nameable {
         return String(describing: Self.self)
     }
     
+    public func addAccessibilityIdentifiers() {
+        for (label, object) in Mirror(reflecting: self).children {
+            guard let label = label else { continue }
+            let accessibleObject: UIAccessibilityIdentification
+            if let view = object as? UIView {
+                accessibleObject = view
+            } else if let barItem = object as? UIBarItem {
+                accessibleObject = barItem
+            } else {
+                continue
+            }
+            accessibleObject.accessibilityIdentifier = Self.name + "." + label
+        }
+    }
+
 }
