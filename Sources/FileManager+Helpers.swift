4bf8e07a774eb200fd6efd92727f3badd439d37e
Ben Norris <code@bsn.design>
2018-09-24 13:43:24 -0600

    Add more FileManager helpers

--- Sources/FileManager+Helpers.swift
+++ Sources/FileManager+Helpers.swift
@@ -21,4 +21,21 @@ public extension FileManager {
         return try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
     }
     
+    public static func copyDefaultItem(named name: String, to destination: URL, forceOverwrite: Bool = false) throws {
+        guard let origin = Bundle.main.url(forResource: name, withExtension: nil) else { throw CocoaError(.fileNoSuchFile) }
+        guard !FileManager.default.fileExists(atPath: destination.path) else {
+            guard forceOverwrite else { return }
+            try FileManager.default.removeItem(at: destination)
+            try FileManager.default.copyItem(at: origin, to: destination)
+            return
+        }
+        try FileManager.default.copyItem(at: origin, to: destination)
+    }
+    
+    public static func createDefaultDirectory(at destination: URL) throws {
+        var isDirectory: ObjCBool = true
+        guard !FileManager.default.fileExists(atPath: destination.path, isDirectory: &isDirectory) else { return }
+        try FileManager.default.createDirectory(at: destination, withIntermediateDirectories: true, attributes: nil)
+    }
+
 }
